62bb932d1a16e53a955c3192a40fbaecd2efeed1
Aaron Lathrop <alathrop78@gmail.com>
2022-02-06 08:49:33 -0700

    add firebase; fix small bugs

--- src/components/TypeAhead.svelte
+++ src/components/TypeAhead.svelte
@@ -17,23 +17,25 @@
     }
 </script>
 
-<input type="search"
-        list='options' 
-        placeholder={placeholder}
-        class='text-lg border-2 w-11/12 p-3 mx-4 mt-4'
-        bind:value={value}
-        on:keydown={e => e.key == 'Enter' && selectOption(value)}
-/>
-<datalist id="options">
-    {#each filteredOptions as option}
-        <option value={option}>{option}</option>
-    {/each}
-</datalist>
-{#if value}
-    <div class="text-center">
-        <button type="submit" class="text-lg border rounded-md shadow-md bg-green-300 hover:bg-green-400 hover:shadow-lg py-3 w-3/4 my-5"
-        on:click={() => selectOption(value)}>
-            Add
-        </button>
-    </div>
-{/if}
+<div class="text-center">
+    <input type="search"
+            list='options' 
+            placeholder={placeholder}
+            class='text-lg border-2 w-11/12 p-3 mx-4 mt-4'
+            bind:value={value}
+            on:keydown={e => e.key == 'Enter' && selectOption(value)}
+    />
+    <datalist id="options">
+        {#each filteredOptions as option}
+            <option value={option}>{option}</option>
+        {/each}
+    </datalist>
+    {#if value}
+        <div class="text-center">
+            <button type="submit" class="text-lg border rounded-md shadow-md bg-green-300 hover:bg-green-400 hover:shadow-lg py-3 w-3/4 my-5"
+            on:click={() => selectOption(value)}>
+                Add
+            </button>
+        </div>
+    {/if}
+</div>
